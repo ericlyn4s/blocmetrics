f6db60a54725d3f29efdca2746d4aa95a469152c
Eric Peterson <ericmpeterson04@gmail.com>
2017-12-27 19:35:38 -0700

    Got code into program for checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
   get 'events/index'
 
   get 'events/show'
-  
+
   resources :registered_applications, except: [:index]
 
   devise_for :users
   get 'welcome/index'
 
   root 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+
+    resources :events, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
